99a01805dae2b70322f1128c35f9fb23cb1ab343
Sneha Sahu <cse23.snehasahu@ggct.co.in>
2026-03-10 21:00:03 +0530

    Added DSA Array Print Program

---
+++ DSA/ARRAY/ArrayPrint.java
@@ -0,0 +1,24 @@
+package DSA.ARRAY;
+import java.util.Scanner;
+
+class ArrayPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[10];
+
+        System.out.println("Enter 10 numbers:");
+
+        for(int i = 0; i<10; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("The Array is:");
+        for(int i = 0; i<10;i++){
+            System.out.println(arr[i]);
+        }
+        sc.close();
+
+
+    }
+}
