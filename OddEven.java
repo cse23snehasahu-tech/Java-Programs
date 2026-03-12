cd9c383c9c508a42a485af0cfe19c401e3953f6d
Sneha Sahu <cse23.snehasahu@ggct.co.in>
2026-03-12 19:12:23 +0530

    DSA: Array Traversal Program

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+ import java.util.Scanner;
+    public class OddEven
+    {
+        public static void main(String args[])
+        {
+            int a;
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter Number:");
+            a=sc.nextInt();
+            if(a%2==0)
+                System.out.println("Even");
+            else
+                System.out.println("Odd");
+            sc.close();
+        }
+    }
+
+
